289227235a33f6bef070796919060d699225074a
Seoryeon <79080533+sryeon1021@users.noreply.github.com>
2021-03-15 18:08:41 +0900

    Add files via upload

---
+++ dowhile.js
@@ -0,0 +1,5 @@
+ans=0
+do {
+    var ans = parseInt(prompt('1+1=?'));
+} while (ans !=2);
+console.log('맞혔습니다!');
---
+++ if.js
@@ -0,0 +1,14 @@
+var a=1;
+var b=2;
+var c= a+b;
+if (Boolean type 조건) {
+  //조건이 참일때만
+  //코드실행
+}
+if (true){
+  console.log('이 구문은실행됩니다');
+}
+
+if (false) {
+  console.log('이 구문은 실행되지 않습니다');
+}
